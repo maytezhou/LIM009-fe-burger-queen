e2a58b8f1a7c7acdc6bf453e9b2672a5297d2f62
NardaC <nardac6@gmail.com>
2019-07-16 19:13:55 -0500

    mostrando desayuno y almuerzo

--- burger-queen/src/components/Almuerzo.js
+++ burger-queen/src/components/Almuerzo.js
@@ -1,19 +1,58 @@
 import React, { useState } from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import app from '../components/Firebase'
 
-const Almuerzo = (arrObj) => {
+
+
+const Almuerzo = () => {
     const [tipo, setTipo] = useState('')
-   
-    
+    const [value, loading, error] = useCollection(
+      app.firestore().collection('almuerzoYcena'),
+      {
+        snapshotListenOptions: { includeMetadataChanges: true },
+      }
+    );
+     
     return (
-    <>
-        <div>
-            <input onClick={() => {setTipo('res')}} type="checkbox" id="gridCheckRes"/>
-            <label for="gridCheckRes">Hamburguesa de Res</label>
-            <input onClick={() => {setTipo('pollo')}} type="checkbox" id="gridCheckChicken"/>
-            <label for="gridCheckChicken">Hamburguesa de Pollo</label>
-            <input  onClick={() => {setTipo('vegetariana')}} type="checkbox" id="gridCheckGreen"/>
-            <label for="gridCheckGreen">Hamburguesa Vegetariana</label>
+      <div>
+          {error && <strong>Error: {error}</strong>}
+          {loading && <span>Collection: Loading...</span>}
+          {value && (
+            <span>
+              {' '}
+
+              {value.docs.filter((ele)=>ele.data().category==='hamburguesa' && ele.data().type==='simple'?ele:'').map(doc => (
+                //<div key={doc.data().id}>{doc.data().name} {doc.data().price}</div>
+                <div>
+            { doc.data().name==='Hamburguesa de res'
+            ?
+            <>
+                <input onClick={() => {setTipo('res')}} type="checkbox" id="gridCheckRes"/>
+                <label htmlFor="gridCheckRes">{doc.data().name ==='Hamburguesa de res' ?doc.data().name:'' }</label>
+            </>
+             :
+             ''}
+              { doc.data().name==='Hamburguesa de pollo'
+            ?
+            <>
+                <input onClick={() => {setTipo('pollo')}} type="checkbox" id="gridCheckChicken"/>
+                <label htmlFor="gridCheckChicken">{doc.data().name ==='Hamburguesa de pollo' ?doc.data().name:'' }</label>
+            </>
+             :
+             ''}
+              { doc.data().name==='Hamburguesa vegetariana'
+            ?
+            <>
+                <input onClick={() => {setTipo('vegetariana')}} type="checkbox" id="gridCheckGreen"/>
+                <label htmlFor="gridCheckGreen">{doc.data().name ==='Hamburguesa vegetariana' ?doc.data().name:'' }</label>
+            </>
+             :
+             ''}
         </div>
+              
+              ))}
+            </span>
+          )}
         <div>
             {tipo === 'res' && (
                 <div>
@@ -31,8 +70,43 @@ const Almuerzo = (arrObj) => {
                 </div>
             )}
         </div>
-    </>
+      </div>
+    );
+  };
 
-    )
-}
+// const Almuerzo = () => {
+//     const [tipo, setTipo] = useState('')
+   
+    
+//     return (
+//     <>
+        // <div>
+        //     <input onClick={() => {setTipo('res')}} type="checkbox" id="gridCheckRes"/>
+        //     <label for="gridCheckRes">Hamburguesa de Res</label>
+        //     <input onClick={() => {setTipo('pollo')}} type="checkbox" id="gridCheckChicken"/>
+        //     <label for="gridCheckChicken">Hamburguesa de Pollo</label>
+        //     <input  onClick={() => {setTipo('vegetariana')}} type="checkbox" id="gridCheckGreen"/>
+        //     <label for="gridCheckGreen">Hamburguesa Vegetariana</label>
+        // </div>
+        // <div>
+        //     {tipo === 'res' && (
+        //         <div>
+        //             res
+        //         </div>
+        //     )}
+        //     {tipo === 'pollo' && (
+        //         <div>
+        //             pollo
+        //         </div>
+        //     )}
+        //     {tipo === 'vegetariana' && (
+        //         <div>
+        //             vegetariana
+        //         </div>
+        //     )}
+        // </div>
+//     </>
+
+//     )
+// }
 export default Almuerzo;
--- burger-queen/src/components/Desayuno.js
+++ burger-queen/src/components/Desayuno.js
@@ -1,32 +1,62 @@
 import React from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import app from '../components/Firebase'
 
-const Desayuno = () => (
-    <div>
-        <table className="table">
-            <tbody>
-                <tr>
-                    <td>Cafe Americano</td>
-                    <td>$5</td>
-                    <td><button type="button" className="btn btn-success">Agregar</button></td>
-                </tr>
-                <tr>
-                    <td>Cafe con leche </td>
-                    <td>$7</td>
-                    <td><button type="button" className="btn btn-success">Agregar</button></td>
-                </tr>
-                <tr>
-                    <td>Sandwich jamon con queso </td>
-                    <td>$10</td>
-                    <td><button type="button" className="btn btn-success">Agregar</button></td>
-                </tr>
-                <tr>
-                    <td>Jugo de frutas natural </td>
-                    <td>$7</td>
-                    <td><button type="button" className="btn btn-success">Agregar</button></td>
-                </tr>
 
-            </tbody>
-        </table>
-    </div>
-)
+
+
+const Desayuno = () => {
+    const [value, loading, error] = useCollection(
+      app.firestore().collection('desayuno'),
+      {
+        snapshotListenOptions: { includeMetadataChanges: true },
+      }
+    );
+     
+    return (
+      <div>
+          {error && <strong>Error: {error}</strong>}
+          {loading && <span>Collection: Loading...</span>}
+          {value && (
+            <span>
+              {' '}
+              {value.docs.map(doc => (
+                <div key={doc.data().id}>{doc.data().name} {doc.data().price}</div>
+              ))}
+            </span>
+          )}
+      </div>
+    );
+  };
+
+
+// const Desayuno = () => (
+//     <div>
+//         <table className="table">
+//             <tbody>
+//                 <tr>
+//                     <td>Cafe Americano</td>
+//                     <td>$5</td>
+//                     <td><button type="button" className="btn btn-success">Agregar</button></td>
+//                 </tr>
+//                 <tr>
+//                     <td>Cafe con leche </td>
+//                     <td>$7</td>
+//                     <td><button type="button" className="btn btn-success">Agregar</button></td>
+//                 </tr>
+//                 <tr>
+//                     <td>Sandwich jamon con queso </td>
+//                     <td>$10</td>
+//                     <td><button type="button" className="btn btn-success">Agregar</button></td>
+//                 </tr>
+//                 <tr>
+//                     <td>Jugo de frutas natural </td>
+//                     <td>$7</td>
+//                     <td><button type="button" className="btn btn-success">Agregar</button></td>
+//                 </tr>
+
+//             </tbody>
+//         </table>
+//     </div>
+// )
 export default Desayuno;
