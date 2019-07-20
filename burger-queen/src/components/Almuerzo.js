c7493399c11b53e87140bfd38e521daea5f6e3cc
NardaC <nardac6@gmail.com>
2019-07-19 20:06:33 -0500

    refactorizando estructura de datos

--- burger-queen/src/components/Almuerzo.js
+++ burger-queen/src/components/Almuerzo.js
@@ -10,7 +10,7 @@ const Almuerzo = () => {
     const [producto, setProducto] = useState(null)
     const [adicional, setAdicional] = useState(null)
     const [value, loading, error] = useCollection(
-      app.firestore().collection('almuerzoYcena'),
+      app.firestore().collection('producto3'),
       {
         snapshotListenOptions: { includeMetadataChanges: true },
       }
@@ -25,23 +25,38 @@ const Almuerzo = () => {
                 {''}
                 {value.docs.filter(
                     (ele) =>
-                        ele.data().category === 'hamburguesa'
-                        && ele.data().type === 'simple'
+                        ele.data().menuType === 'almuerzoYcena'
                         && ele
-                ).map((doc)=>
+                ).map((doc) =>
+                    
                     <div key={doc.data().id}>
                             <input onClick={() => {setTipo(doc.data().name)}} type="radio" id="gridCheckRes" name='hamburguesas'/>
                             <label htmlFor="gridCheckRes">{doc.data().name}</label>
                             {tipo === doc.data().name && (
                                 <>
                                 <div>
-                                    <button type="button" className="btn btn-success" onClick={() =>{setAdicional(doc.data().type)}}>Simple ${doc.data().price}</button>
-                          
+                                    <button type="button" className="btn btn-success" onClick={() =>{setAdicional(doc.data().type.simple)}}>Simple ${doc.data().type.simple}</button>
+                            {adicional === doc.data().type.simple &&(
+                                <>
+                                <div > 
+                                    Solo simple ${doc.data().type.simple}
+                                    <button>Agregar </button>
+                                </div>
+                                <div>
+                                    {doc.data().category === 'adicional'
+                                    && doc.data().name === 'Con huevo'
+                                    && doc.data().price}
+                                    <button>Agregar</button>
+                                </div>
+                                <div>Queso + 
+                                    <button>Agregar</button>
+                                </div>
+                                </>
+                            )}
                                 </div>
                                 <div>    
                                     <button type="button" className="btn btn-success">Doble ${doc.data().price}</button>
                                 </div>
-                                <Simple/>
                                 </>
                             )}
                     </div>
@@ -54,153 +69,69 @@ const Almuerzo = () => {
 };
 
 
-
-//<span>
-                // {value.docs.filter((ele) => 
-                // ele.data().category==='hamburguesa' 
-                // && ele.data().type==='simple'
-                // && ele).map(doc => (
-                //     <div key={doc.data().id}>
-                //         {doc.data().name==='Hamburguesa de res' && (
-                            // <>
-                            // <input onClick={() => {setTipo('res'); setProducto(doc.data())}} type="radio" id="gridCheckRes" name='hamburguesas'/>
-                            // <label htmlFor="gridCheckRes">{doc.data().name ==='Hamburguesa de res' ?doc.data().name:'' }</label>
-                            // </>
-                //         )}
-                //         {doc.data().name==='Hamburguesa de pollo' && (
-                //             <>
-                //             <input onClick={() => {setTipo('pollo')}} type="radio" id="gridCheckChicken" name='hamburguesas'/>
-                //             <label htmlFor="gridCheckChicken">{doc.data().name ==='Hamburguesa de pollo' ?doc.data().name:'' }</label>
-                //             </>
-                //         )}
-                //         {doc.data().name==='Hamburguesa vegetariana' && (
-                //             <>
-                //             <input onClick={() => {setTipo('vegetariana')}} type="radio" id="gridCheckGreen" name='hamburguesas'/>
-                //             <label htmlFor="gridCheckGreen">{doc.data().name ==='Hamburguesa vegetariana' ?doc.data().name:'' }</label>
-                //             </>
-                //         )}
-                //     </div>
-                // ))
-                // }
-                        // <div>
-                            // {tipo === 'res' && (
-                            //     <div>
-                            //         {value.docs.map((ele) => (
-                            //             ele.data().category==='hamburguesa'
-                            //             && ele.data().name==='Hamburguesa de res'
-                            //             && ele.data().type==='simple'
-                            //             && (
-                            //                 <button type="button" className="btn btn-success" onClick={setAdicional('simple')}>Simple ${ele.data().price}</button>
-                            //             )
-                            //         ))}
-                            //         {value.docs.map((ele)=>ele.data().category==='hamburguesa' 
-                            //             && ele.data().name==='Hamburguesa de res'
-                            //             && ele.data().type==='doble' 
-                            //             && (   
-                            //                 <button type="button" className="btn btn-success">Doble ${ele.data().price}</button>
-                            //             )
-                            //         )}
-                            //     </div>
-                            // )}
-                //             {tipo === 'pollo' && (
-                //                 <div>
-                //                     {value.docs.map((ele)=>
-                //                         ele.data().category==='hamburguesa' 
-                //                         && ele.data().name==='Hamburguesa de pollo'
-                //                         && ele.data().type==='simple'
-                //                         && (
-                //                         <button type="button" className="btn btn-primary">Simple ${ele.data().price}</button>
-                //                         )
-                //                     )}
-                //                     {value.docs.map((ele)=>ele.data().category==='hamburguesa' 
-                //                         && ele.data().name==='Hamburguesa de pollo'
-                //                         && ele.data().type==='doble'
-                //                         &&  (
-                //                             <button type="button" className="btn btn-primary">Doble ${ele.data().price}</button>
-                //                         )
-                //                     )}
-                //                 </div>
-                //             )}
-                //             {tipo === 'vegetariana' && (
-                //                 <div>
-                //                     {value.docs.map((ele)=>ele.data().category==='hamburguesa' 
-                //                         && ele.data().name==='Hamburguesa vegetariana'
-                //                         && ele.data().type==='simple'
-                //                         && (
-                //                             <button type="button" className="btn btn-danger">Simple ${ele.data().price}</button>
-                //                         )
-                //                     )}
-                //                     {value.docs.map((ele)=>ele.data().category==='hamburguesa' 
-                //                         && ele.data().name==='Hamburguesa vegetariana'
-                //                         && ele.data().type==='doble'
-                //                         && (
-                //                             <button type="button" className="btn btn-danger">Doble ${ele.data().price}</button>
-                //                         )
-                //                     )}
-                //                 </div>
-                //             )}
-                //         </div>
-                //         <div>
-                //             {adicional === 'simple' && (
-                //                 <Simple precio1={producto.price}/>
-                //             )}
-                            
-                //             {adicional === 'huevo' && (
-                //                 <Simple/>
-                //             )}
-                //              {adicional === 'queso' && (
-                //                 <Simple/>
-                //             )}
-                               
-                           
-                //         </div>
-                // </span>
-
-
-
-
-
-
-
-
-
-
-
+export default Almuerzo;
 
 
 // const Almuerzo = () => {
 //     const [tipo, setTipo] = useState('')
-   
-    
-//     return (
-//     <>
-        // <div>
-        //     <input onClick={() => {setTipo('res')}} type="checkbox" id="gridCheckRes"/>
-        //     <label for="gridCheckRes">Hamburguesa de Res</label>
-        //     <input onClick={() => {setTipo('pollo')}} type="checkbox" id="gridCheckChicken"/>
-        //     <label for="gridCheckChicken">Hamburguesa de Pollo</label>
-        //     <input  onClick={() => {setTipo('vegetariana')}} type="checkbox" id="gridCheckGreen"/>
-        //     <label for="gridCheckGreen">Hamburguesa Vegetariana</label>
-        // </div>
-        // <div>
-        //     {tipo === 'res' && (
-        //         <div>
-        //             res
-        //         </div>
-        //     )}
-        //     {tipo === 'pollo' && (
-        //         <div>
-        //             pollo
-        //         </div>
-        //     )}
-        //     {tipo === 'vegetariana' && (
-        //         <div>
-        //             vegetariana
-        //         </div>
-        //     )}
-        // </div>
-//     </>
+//     const [producto, setProducto] = useState(null)
+//     const [adicional, setAdicional] = useState(null)
+//     const [value, loading, error] = useCollection(
+//       app.firestore().collection('productos'),
+//       {
+//         snapshotListenOptions: { includeMetadataChanges: true },
+//       }
+//     );
 
-//     )
-// }
-export default Almuerzo;
+//     return (
+//         <div>
+//             {error && <strong>Error: {error}</strong>}
+//             {loading && <span>Collection: Loading...</span>}
+//             {value && (
+//                 <>
+//                 {''}
+//                 {value.docs.filter(
+//                     (ele) =>
+//                         ele.data().menuType === 'almuerzoYcena'
+//                         && ele.data().category === 'hamburguesa'
+//                         && ele.data().type === 'simple'
+//                         && ele
+//                 ).map((doc) =>
+//                     <div key={doc.data().id}>
+//                             <input onClick={() => {setTipo(doc.data().name)}} type="radio" id="gridCheckRes" name='hamburguesas'/>
+//                             <label htmlFor="gridCheckRes">{doc.data().name}</label>
+//                             {tipo === doc.data().name && (
+//                                 <>
+//                                 <div>
+//                                     <button type="button" className="btn btn-success" onClick={() =>{setAdicional(doc.data().type)}}>Simple ${doc.data().price}</button>
+//                             {adicional === doc.data().type &&(
+//                                 <>
+//                                 <div > 
+//                                     Solo simple ${doc.data().price}
+//                                     <button>Agregar </button>
+//                                 </div>
+//                                 <div>
+//                                     {doc.data().category === 'adicional'
+//                                     && doc.data().name === 'Con huevo'
+//                                     && doc.data().price}
+//                                     <button>Agregar</button>
+//                                 </div>
+//                                 <div>Queso + 
+//                                     <button>Agregar</button>
+//                                 </div>
+//                                 </>
+//                             )}
+//                                 </div>
+//                                 <div>    
+//                                     <button type="button" className="btn btn-success">Doble ${doc.data().price}</button>
+//                                 </div>
+//                                 </>
+//                             )}
+//                     </div>
+//                 )
+//                }
+//                </>
+//             )}
+//         </div>
+//         );
+// };
--- burger-queen/src/components/Desayuno.js
+++ burger-queen/src/components/Desayuno.js
@@ -7,7 +7,7 @@ import app from '../components/Firebase'
 
 const Desayuno = () => {
     const [value, loading, error] = useCollection(
-      app.firestore().collection('desayuno'),
+      app.firestore().collection('productos2'),
       {
         snapshotListenOptions: { includeMetadataChanges: true },
       }
@@ -18,45 +18,29 @@ const Desayuno = () => {
           {error && <strong>Error: {error}</strong>}
           {loading && <span>Collection: Loading...</span>}
           {value && (
-            <span>
-              {' '}
-              {value.docs.map(doc => (
-                <div key={doc.data().id}>{doc.data().name} {doc.data().price}</div>
-              ))}
-            </span>
+                <span>
+                {value.docs.map(ele => (
+                    <>
+                    <div>
+                        {ele.data().americano.name} ${ele.data().americano.price}
+                    </div>
+                    <div>
+                        {ele.data().leche.name} ${ele.data().leche.price}
+                    </div>
+                    <div>
+                        {ele.data().sandwich.name} ${ele.data().sandwich.price}
+                    </div>
+                    <div>
+                        {ele.data().jugo.name} ${ele.data().jugo.price}
+                    </div>
+                    </>
+                )
+                )}
+                </span>
           )}
       </div>
     );
   };
 
 
-// const Desayuno = () => (
-//     <div>
-//         <table className="table">
-//             <tbody>
-//                 <tr>
-//                     <td>Cafe Americano</td>
-//                     <td>$5</td>
-//                     <td><button type="button" className="btn btn-success">Agregar</button></td>
-//                 </tr>
-//                 <tr>
-//                     <td>Cafe con leche </td>
-//                     <td>$7</td>
-//                     <td><button type="button" className="btn btn-success">Agregar</button></td>
-//                 </tr>
-//                 <tr>
-//                     <td>Sandwich jamon con queso </td>
-//                     <td>$10</td>
-//                     <td><button type="button" className="btn btn-success">Agregar</button></td>
-//                 </tr>
-//                 <tr>
-//                     <td>Jugo de frutas natural </td>
-//                     <td>$7</td>
-//                     <td><button type="button" className="btn btn-success">Agregar</button></td>
-//                 </tr>
-
-//             </tbody>
-//         </table>
-//     </div>
-// )
 export default Desayuno;
