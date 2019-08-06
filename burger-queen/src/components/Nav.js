c6f4f798b6a765fe5c8efd195e558ed464aa71a0
maytezhou <you@example.com>
2019-08-06 09:36:15 -0500

    Updates

--- burger-queen/src/components/Nav.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Nav = ({ logo }) => (
-  <header>
-    <img data-testid="imgLogo" className="center" src={logo} alt="logo"></img>
-  </header>
-);
-export default Nav;
--- burger-queen/src/components/Total.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Total = ({ pedidos, gettingTotalCost }) => (
-   <div>total = ${gettingTotalCost(pedidos)}</div>
-);
-
-export default Total;
